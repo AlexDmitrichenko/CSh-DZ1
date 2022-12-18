f9366271431ed7c662d1201c4ae57d7aad6f6ad5
Alex <best1972@list.ru>
2022-12-18 16:14:33 +0300

    made task 3

--- CSh DZ1/Program.cs
+++ CSh DZ1/Program.cs
@@ -11,7 +11,7 @@ else {
   Console.Write($"Max = {num2}");
 }*/
 //Задача 2
-Console.WriteLine("Введите число 1: ");
+/*Console.WriteLine("Введите число 1: ");
 int numb1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число 2: ");
 int numb2 = int.Parse(Console.ReadLine()!);
@@ -21,4 +21,14 @@ int max = numb1;
 
 if (numb2 > max) max = numb2; 
 if (numb3 > max) max = numb3; 
-Console.Write($"Max = {max}");
+Console.Write($"Max = {max}");*/
+//Задача 3
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number%2! == 0){
+    Console.Write($"Число являеться чётным");
+}
+else {
+  Console.Write($"Число являеться не чётным");
+}
