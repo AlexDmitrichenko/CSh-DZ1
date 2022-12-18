7e3d4b13745d7d00ca72065c87ff3839855e8b75
Alex <best1972@list.ru>
2022-12-18 10:00:24 +0300

    make pr 2

--- CSh S1/Program.cs
+++ CSh S1/Program.cs
@@ -3,8 +3,19 @@
 /*Многострочный
 коментарий или не активный код*/
 
-// Задача Программа принимает на вход число и возводит его в квадрат (sqr)
-Console.WriteLine("Введите число: ");
+// Задача 1 Программа принимает на вход число и возводит его в квадрат (sqr)
+/*Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 int sqr = Convert.ToInt32(Math.Pow(number, 2));
-Console.WriteLine($"Число {number} в квадрате равно {sqr}");
+Console.WriteLine($"Число {number} в квадрате равно {sqr}");*/
+// Задача 2 Программа принимает на вход одно число (N),
+//а на выходе показывает все целые числа в промежутке от (-N до N)
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine()!);
+
+int numb1 = -numb;
+
+while (numb1 <= numb){
+   Console.Write($"{numb1}, ");
+   numb1++;
+}
