ce50c7ce530d4c8da814acdfbc6f41796f4f17ad
Alex <best1972@list.ru>
2022-12-18 16:06:16 +0300

    made task 2

--- CSh DZ1/Program.cs
+++ CSh DZ1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 1
-Console.WriteLine("Введите число 1: ");
+/*Console.WriteLine("Введите число 1: ");
 int num1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число 2: ");
 int num2 = int.Parse(Console.ReadLine()!);
@@ -9,4 +9,16 @@ if (num1 > num2){
 }
 else {
   Console.Write($"Max = {num2}");
-}
+}*/
+//Задача 2
+Console.WriteLine("Введите число 1: ");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int numb2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 3: ");
+int numb3 = int.Parse(Console.ReadLine()!);
+int max = numb1;
+
+if (numb2 > max) max = numb2; 
+if (numb3 > max) max = numb3; 
+Console.Write($"Max = {max}");
