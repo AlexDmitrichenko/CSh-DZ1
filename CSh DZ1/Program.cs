3b1a14df0845bdbf1d43b34a1361cfba072f8a3e
Alex <best1972@list.ru>
2022-12-18 15:43:47 +0300

    made task 1

---
+++ CSh DZ1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2){
+  Console.Write($"Max = {num1}");
+}
+else {
+  Console.Write($"Max = {num2}");
+}
